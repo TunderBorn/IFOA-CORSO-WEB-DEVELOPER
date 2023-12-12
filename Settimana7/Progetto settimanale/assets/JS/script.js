7402618c757bfe12b4b14a9f79122ac2133b6581
TunderBorn <orione.pegaso@gmail.com>
2023-12-12 18:17:40 +0100

    Progetto Settimanale 7

---
+++ Settimana7/Progetto settimanale/assets/JS/script.js
@@ -0,0 +1,101 @@
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+
+const key =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4MjgzN2MwNTgzNTAwMTg1MjJkMDciLCJpYXQiOjE3MDIzNzM0MzIsImV4cCI6MTcwMzU4MzAzMn0.jQ_QpwdN8o3YSgrCTdQ4GgznCmmZhf31zC7qlmU9FW8";
+const headers = {
+  Authorization: key,
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+fetch(url, {
+  headers: headers,
+}).then((response) =>
+  response.json().then((data) => {
+    products = data;
+    createCards(data);
+  })
+);
+
+/* FUNZIONI PAGINA HOMEPAGE */
+function createCards(data) {
+  data.forEach((data) => {
+    let col = document.createElement("div");
+    col.classList.add("col");
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let img = document.createElement("img");
+    img.classList.add("card-img-top", "object-fit-cover");
+    img.src = data.imageUrl;
+    img.style.height = "15rem";
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    let cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerText = data.name;
+    let cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.innerText = data.brand;
+    let row = document.getElementById("prodotti");
+
+    /*buttons*/
+    let viewBtn = document.createElement("button");
+    viewBtn.classList.add("btn", "btn-outline-secondary"); // data-bs-toggle="modal"
+    viewBtn.innerText = "Scopri";
+
+    let editBtn = document.createElement("button");
+    editBtn.classList.add("btn", "btn-outline-secondary"); // data-bs-toggle="modal"
+    editBtn.innerText = "Modifica";
+
+    /*APPEND CHILD*/
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(viewBtn);
+    cardBody.appendChild(editBtn);
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    row.appendChild(card);
+  });
+}
+
+/* FUNZIONI PAGINA BACK OFFICE*/
+const BtnReset = document.getElementById("BtnReset");
+const BtnNew = document.getElementById("BtnNew");
+const productNew = [];
+
+function newCards() {
+  const Nome = document.getElementById("nome").value;
+  const Brand = document.getElementById("brand").value;
+  const Prezzo = document.getElementById("prezzo").value;
+  const urlImmagine = document.getElementById("urlImmagine").value;
+  const Descrizione = document.getElementById("descrizione").value;
+
+  post();
+}
+newCards(Nome, Descrizione, Brand, urlImmagine, Prezzo);
+
+
+function post(Nome, Descrizione, Brand, urlImmagine, Prezzo) {
+  fetch(url, {
+      method: 'POST',
+      headers: {
+          Authorization: key,
+          Accept: "application/json",
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                  'name': Nome,
+                  'description': Descrizione,
+                  'brand': Brand,
+                  'imageUrl': urlImmagine,
+                  'price': Prezzo
+              })
+  })
+  .then((response) => response.json())
+    .then(data => {
+      newCards(data);
+    })
+  }
+
+
+/* FUNZIONI PAGINA EDIT*/
